fe2127815910f227bf9e636296f592c1cb11274b
David Vernon <David@VernonAndCo.com>
2024-04-23 14:39:08 -0400

    Make subproject repository location configuration easier  &  Condense fetch scripts

--- scripts/fetchComponents.ps1
+++ scripts/fetchComponents.ps1
@@ -1,23 +1,32 @@
-# Delete the gpt-rag-ingestion folder from .azure if it exists
-if (Test-Path -Path ".\.azure\gpt-rag-ingestion") {
-    Remove-Item -Path ".\.azure\gpt-rag-ingestion" -Recurse -Force
+#!/usr/bin/env pwsh
+
+$reposGitCloneArguments = @(
+    # Per-repository strings  of  repository-specific `git clone` arguments
+    #   ending with the  Repository URL  with optional .git suffix,
+    # including AT LEAST  3 strings
+    #   ending with  EACH of the following repository/directory names  EXACTLY:
+    #
+    #     gpt-rag-ingestion
+    #     gpt-rag-orchestrator
+    #     gpt-rag-frontend
+    'https://github.com/Azure/gpt-rag-ingestion',
+    'https://github.com/Azure/gpt-rag-orchestrator',
+    'https://github.com/Azure/gpt-rag-frontend'
+)
+
+#$gitCloneOptions = '--single-branch'  # for all repositories
+
+foreach ($repoCloneArgs in $reposGitCloneArguments) {
+
+    ''
+
+    # Delete the repository folder from .azure if it exists
+    path = "./.azure/$(Split-Path -LeafBase $repoCloneArgs)"
+    #                  e.g., gpt-rag-ingestion from https://github.com/Azure/gpt-rag-ingestion
+    if (Test-Path -PathType Container  $path) {
+        Remove-Item -Recurse -Force    $path
+    }
+
+    # Clone the repository into the .azure folder
+    Invoke-Expression "git clone $gitCloneOptions $repoCloneArgs '$path'"
 }
-
-# Clone the repository into the .azure folder
-git clone https://github.com/Azure/gpt-rag-ingestion .\.azure\gpt-rag-ingestion
-
-# Delete the gpt-rag-orchestrator folder from .azure if it exists
-if (Test-Path -Path ".\.azure\gpt-rag-orchestrator") {
-    Remove-Item -Path ".\.azure\gpt-rag-orchestrator" -Recurse -Force
-}
-
-# Clone the repository into the .azure folder
-git clone https://github.com/Azure/gpt-rag-orchestrator .\.azure\gpt-rag-orchestrator
-
-# Delete the gpt-rag-frontend folder from .azure if it exists
-if (Test-Path -Path ".\.azure\gpt-rag-frontend") {
-    Remove-Item -Path ".\.azure\gpt-rag-frontend" -Recurse -Force
-}
-
-# Clone the repository into the .azure folder
-git clone https://github.com/Azure/gpt-rag-frontend .\.azure\gpt-rag-frontend
--- scripts/fetchComponents.sh
+++ scripts/fetchComponents.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 
-# Delete the gpt-rag-ingestion folder from .azure if it exists
-if [ -d ./.azure/gpt-rag-ingestion ]; then
-    rm -rf ./.azure/gpt-rag-ingestion
-fi
+repos_git_clone_arguments=(
+    # Per-repository strings  of  repository-specific `git clone` arguments
+    #   ending with the  Repository URL  with optional .git suffix,
+    # including AT LEAST  3 strings
+    #   ending with  EACH of the following repository/directory names  EXACTLY:
+    #
+    #     gpt-rag-ingestion
+    #     gpt-rag-orchestrator
+    #     gpt-rag-frontend
+    https://github.com/Azure/gpt-rag-ingestion
+    https://github.com/Azure/gpt-rag-orchestrator
+    https://github.com/Azure/gpt-rag-frontend
+)
 
-# Clone the repository into the .azure folder
-git clone https://github.com/Azure/gpt-rag-ingestion ./.azure/gpt-rag-ingestion
+#git_clone_options=--single-branch  # for all repositories
 
-# Delete the gpt-rag-orchestrator folder from .azure if it exists
-if [ -d ./.azure/gpt-rag-orchestrator ]; then
-    rm -rf ./.azure/gpt-rag-orchestrator
-fi
+for repo_clone_args in "${repos_git_clone_arguments[@]}"; do
 
-# Clone the repository into the .azure folder
-git clone https://github.com/Azure/gpt-rag-orchestrator ./.azure/gpt-rag-orchestrator
+    echo
 
-# Delete the gpt-rag-frontend folder from .azure if it exists
-if [ -d ./.azure/gpt-rag-frontend ]; then
-    rm -rf ./.azure/gpt-rag-frontend
-fi
+    # Delete the repository folder from .azure if it exists
+    path=./.azure/$(basename --suffix=.git -- "$repo")
+    #               e.g., gpt-rag-ingestion from https://github.com/Azure/gpt-rag-ingestion
+    if [ -d    "$path" ]; then
+        rm -rf "$path"
+    fi
 
-# Clone the repository into the .azure folder
-git clone https://github.com/Azure/gpt-rag-frontend ./.azure/gpt-rag-frontend
+    # Clone the repository into the .azure folder
+    git clone $git_clone_options $repo_clone_args "$path"
+done
